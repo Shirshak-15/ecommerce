4c57b91a4c2c9a04da8ddeb1273b3e2afadab651
shirshak saha <shirshak.saha@indusnet.co.in>
2024-07-04 13:23:38 +0530

    the styling of groceries page has been done

--- src/Groceries.js
+++ src/Groceries.js
@@ -4,20 +4,21 @@ import React from 'react';
 const Groceries = (props) => {
     const handleclick = (newitem) => {
         props.setcart(previtem=>[...previtem, newitem]);
+        
     }
 
     return (
         <div>
         <div className="centerDiv">GROCERIES</div>
         
-        {props.list.map((e)=>{ return<div key={e.name}>
-            <div>{e.name} Rs:{e.price}</div>
+        <div className='itempage'>{props.list.map((e)=>{ return<div className='itemcard'  key={e.name} > 
+            <p>{e.name}</p><p>Rs:{e.price}</p> 
             
-            <button onClick={() => handleclick({ name:`${e.name}` , price: `${e.price}` })}>add</button>
+            <button className='itembutton' onClick={() => handleclick({ name:`${e.name}` , price: `${e.price}` })}>add</button>
 
-            </div>
+               </div>
 
-        })}
+        })}</div>
             
             {console.log(props.cart)}
         </div>
